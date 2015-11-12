b967ca883901cd3ff1bff21639b9499da620ee5a
MinkyuCho(Alan) <mrnoname@coupang.com>
2015-11-12 23:14:05 +0900

    Downgrade from Babel 6

--- client/webpack/babel.config.js
+++ client/webpack/babel.config.js
@@ -1,29 +1,30 @@
 module.exports = {
-  presets: ["es2015", "react", "stage-0"],
-  optional: ['runtime'],
-  stage: 0,
-  plugins: ['typecheck'],
-  env: {
-    development: {
-      plugins: ['react-transform'],
-      extra: {
-        'react-transform': {
-          transforms: [
-            {
-              transform: 'react-transform-hmr',
-              imports: ['react'],
-              locals: ['module']
-            },
-            {
-              transform: 'react-transform-catch-errors',
-              imports: [
-                'react',
-                'redbox-react'
-              ]
-            }
-          ]
-        }
-      }
-    }
-  }
+  "stage": 0,
+  "optional": "runtime",
+  "loose": "all",
+  "plugins": ["typecheck"]
+  //,
+  //"env": {
+  //  "development": {
+  //    "plugins": [
+  //      "react-transform"
+  //    ],
+  //    "extra": {
+  //      "react-transform": {
+  //        "transforms": [
+  //          {
+  //            "transform": "react-transform-hmr",
+  //            "imports": ["react"],
+  //            "locals": ["module"]
+  //          },
+  //          {
+  //            "transform": "react-transform-catch-errors",
+  //            "imports": ["react", "redbox-react"
+  //            ]
+  //          }
+  //        ]
+  //      }
+  //    }
+  //  }
+  //}
 };
